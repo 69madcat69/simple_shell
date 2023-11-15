074f1e6608e38a8fcb712d2e24c2091a750b1fe5
69madcat69 <a7medtarek201577@gmail.com>
2023-11-15 07:06:56 -0800

    life is fun

--- shell.h
+++ shell.h
@@ -194,14 +194,14 @@ void clear_info(info_t *);
 void set_info(info_t *, char **);
 void free_info(info_t *, int);
 
-/* alx_get_env.c */
+/* alx_shell_env.c */
 char *_getenv(info_t *, const char *);
 int _myenv(info_t *);
 int _mysetenv(info_t *);
 int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
 
-/* alx_getenv.c */
+/* alx_get_env.c */
 char **get_env(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
